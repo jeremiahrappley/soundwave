5432f3ee7f34a53bab594e4ac499744e8ff57271
Jeremiah Rappley <jrappley@icloud.com>
2023-11-15 23:18:26 -0500

    implemented non-functioning dark mode toggle

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,6 +1,7 @@
 // create an example NavBar component
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { DarkModeToggle } from './DarkModeToggle';
 import './styles.css';
 
 function NavBar() {
@@ -19,13 +20,7 @@ function NavBar() {
 					<Link to="/2020">2020</Link>
 					<Link to="/2019">2019</Link>
 					<Link to="/2018">2018</Link>
-
-					{/* <a href="./2023.js">2023</a>
-                	<a href="./2022.js">2022</a>
-					<a href="./2021.js">2021</a>
-					<a href="./2020.js">2020</a>
-					<a href="./2019.js">2019</a>
-					<a href="./2018.js">2018</a> */}
+                    <DarkModeToggle />
             	</div>
         	</nav>
 		</div>
