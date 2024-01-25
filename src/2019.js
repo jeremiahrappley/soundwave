3a25e92757060253420f6b04224ae2d7e32bf128
Jeremiah Rappley <jrappley@icloud.com>
2024-01-25 00:47:29 -0500

    WAVE-8 embedded playlists in 2018-2021 years

--- src/2019.js
+++ src/2019.js
@@ -7,18 +7,18 @@ function Playlists2019() {
     return (
         <div className="app">
 			<Header title="2019"></Header>
-			<Playlist title="January 2019" embedUrl="" ></Playlist>
-			<Playlist title="February 2019" embedUrl="" ></Playlist>
-			<Playlist title="March 2019" embedUrl="" ></Playlist>
-			<Playlist title="April 2019" embedUrl="" ></Playlist>
-			<Playlist title="May 2019" embedUrl="" ></Playlist>
-			<Playlist title="June 2019" embedUrl="" ></Playlist>
-			<Playlist title="July 2019" embedUrl="" ></Playlist>
-			<Playlist title="August 2019" embedUrl="" ></Playlist>
-			<Playlist title="September 2019" embedUrl="" ></Playlist>
-			<Playlist title="October 2019" embedUrl="" ></Playlist>
-			<Playlist title="November 2019" embedUrl="" ></Playlist>
-			<Playlist title="December 2019" embedUrl="" ></Playlist>
+			<Playlist title="January 2019" embedUrl="https://embed.music.apple.com/us/playlist/january-2019/pl.u-ZE6lc4aYMz" ></Playlist>
+			<Playlist title="February 2019" embedUrl="https://embed.music.apple.com/us/playlist/february-2019/pl.u-b0p8tbG9pL" ></Playlist>
+			<Playlist title="March 2019" embedUrl="https://embed.music.apple.com/us/playlist/march-2019/pl.u-vV6MsXRYe1" ></Playlist>
+			<Playlist title="April 2019" embedUrl="https://embed.music.apple.com/us/playlist/april-2019/pl.u-AovkskXm97" ></Playlist>
+			<Playlist title="May 2019" embedUrl="https://embed.music.apple.com/us/playlist/may-2019/pl.u-b0M9CbG9pL" ></Playlist>
+			<Playlist title="June 2019" embedUrl="https://embed.music.apple.com/us/playlist/june-2019/pl.u-lD76Iez2Nx" ></Playlist>
+			<Playlist title="July 2019" embedUrl="https://embed.music.apple.com/us/playlist/july-2019/pl.u-6dEmu50ANZ" ></Playlist>
+			<Playlist title="August 2019" embedUrl="https://embed.music.apple.com/us/playlist/august-2019/pl.u-lDGYsez2Nx" ></Playlist>
+			<Playlist title="September 2019" embedUrl="https://embed.music.apple.com/us/playlist/september-2019/pl.u-6dKvF50ANZ" ></Playlist>
+			<Playlist title="October 2019" embedUrl="https://embed.music.apple.com/us/playlist/october-2019/pl.u-6d6Zu50ANZ" ></Playlist>
+			<Playlist title="November 2019" embedUrl="https://embed.music.apple.com/us/playlist/november-2019/pl.u-LVk4ulZbok" ></Playlist>
+			<Playlist title="December 2019" embedUrl="https://embed.music.apple.com/us/playlist/december-2019/pl.u-PeRpFxzKbk" ></Playlist>
 			<Footer/>
         </div>
     );
--- src/2021.js
+++ src/2021.js
@@ -7,18 +7,18 @@ function Playlists2021() {
     return (
         <div className="app">
             <Header title="2021"></Header>
-            <Playlist title="January 2021" embedUrl="" ></Playlist>
-            <Playlist title="February 2021" embedUrl="" ></Playlist>
-            <Playlist title="March 2021" embedUrl="" ></Playlist>
-            <Playlist title="April 2021" embedUrl="" ></Playlist>
-            <Playlist title="May 2021" embedUrl="" ></Playlist>
-            <Playlist title="June 2021" embedUrl="" ></Playlist>
-            <Playlist title="July 2021" embedUrl="" ></Playlist>
-            <Playlist title="August 2021" embedUrl="" ></Playlist>
-            <Playlist title="September 2021" embedUrl="" ></Playlist>
-            <Playlist title="October 2021" embedUrl="" ></Playlist>
-            <Playlist title="November 2021" embedUrl="" ></Playlist>
-            <Playlist title="December 2021" embedUrl="" ></Playlist>
+            <Playlist title="January 2021" embedUrl="https://embed.music.apple.com/us/playlist/january-2021/pl.u-AkkeNSkXm97" ></Playlist>
+            <Playlist title="February 2021" embedUrl="https://embed.music.apple.com/us/playlist/february-2021/pl.u-Ymml0Cko25x" ></Playlist>
+            <Playlist title="March 2021" embedUrl="https://embed.music.apple.com/us/playlist/march-2021/pl.u-AkkLeSkXm97" ></Playlist>
+            <Playlist title="April 2021" embedUrl="https://embed.music.apple.com/us/playlist/april-2021/pl.u-RRRM1uBVP8x" ></Playlist>
+            <Playlist title="May 2021" embedUrl="https://embed.music.apple.com/us/playlist/may-2021/pl.u-RRRARCBVP8x" ></Playlist>
+            <Playlist title="June 2021" embedUrl="https://embed.music.apple.com/us/playlist/june-2021/pl.u-BNNZWCMZv9p" ></Playlist>
+            <Playlist title="July 2021" embedUrl="https://embed.music.apple.com/us/playlist/july-2021/pl.u-BNp7bFMZv9p" ></Playlist>
+            <Playlist title="August 2021" embedUrl="https://embed.music.apple.com/us/playlist/august-2021/pl.u-lexZLFez2Nx" ></Playlist>
+            <Playlist title="September 2021" embedUrl="https://embed.music.apple.com/us/playlist/september-2021/pl.u-AkZgyhkXm97" ></Playlist>
+            <Playlist title="October 2021" embedUrl="https://embed.music.apple.com/us/playlist/october-2021/pl.u-lexoeUez2Nx" ></Playlist>
+            <Playlist title="November 2021" embedUrl="https://embed.music.apple.com/us/playlist/november-2021/pl.u-BNpjVTMZv9p" ></Playlist>
+            <Playlist title="December 2021" embedUrl="https://embed.music.apple.com/us/playlist/december-2021/pl.u-PDRE4uxzKbk" ></Playlist>
             <Footer/>
         </div>
     );
